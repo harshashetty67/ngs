0630e36f60a13f9d456d5d6732a28ebe3ad62084
Harsha Shetty <harsha.shetty@siemens-healthineers.com>
2023-06-12 21:50:33 +0530

    module system

--- 5-web/1-hello-world.js
+++ 5-web/1-hello-world.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require('http'); // require() is of Common-Js module system. we can use => import {createServer} from 'http' => importing the function directly =>EcmaScript module system..
 
 const server = http.createServer((req, res) => {
   res.end('Hello World\n');
